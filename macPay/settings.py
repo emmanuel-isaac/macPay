63ae15ba86feeae5a40bc172bf12f794f3cceb1b
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 02:56:17 +0100

    modify settings module

--- macPay/settings.py
+++ macPay/settings.py
@@ -17,7 +17,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'btzcku%r=l*4r+8ta_x+$yndm2&--%$o^0pehivvs6h4*(5==8'
+import envvars
+envvars.load()
+SECRET_KEY = envvars.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
