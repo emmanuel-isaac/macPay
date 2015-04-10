ffabd321ba051f8470c904b9bc3ad401860c6b32
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 15:53:14 +0100

    Create home view

--- apps/macpayuser/views.py
+++ apps/macpayuser/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from django.views.generic import View
 
 # Create your views here.
+
+
+
+
+class HomeView(View):
+
+    def get(self, request):
+        return render_to_response('index.html')
