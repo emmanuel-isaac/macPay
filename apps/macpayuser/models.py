d722be3353893cb4f8ef2597292723a472d9780d
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 15:52:59 +0100

    Create models in macpayuser app

--- apps/macpayuser/models.py
+++ apps/macpayuser/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import UserManager, User as DjangoUser
 
+
+from apps.computer.models import Computer
+from apps.payment.models import PaymentPlan
+
 # Create your models here.
 
 DjangoUser._meta.get_field('first_name').max_length=50
@@ -13,8 +17,18 @@ DjangoUser._meta.get_field('is_superuser').default=True
 
 
 class StaffUser(models.Model):
-	user = models.OneToOneField(DjangoUser)
+    user = models.OneToOneField(DjangoUser)
 
-	def __str__(self):
-		return '{}'.format(self.user.username)
+    def __str__(self):
+        return '{}'.format(self.user.username)
 
+
+class Fellow(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    payment_start_date = models.DateField(null=True)
+    computer = models.ForeignKey(Computer)
+    payment_plan = models.ForeignKey(PaymentPlan)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name, self.last_name)
