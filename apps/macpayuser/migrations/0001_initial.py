905447e2453d23f2c6a7689b082a700bf0499166
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 15:49:06 +0100

    Create migration for additional models in app

--- apps/macpayuser/migrations/0001_initial.py
+++ apps/macpayuser/migrations/0001_initial.py
@@ -8,10 +8,24 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('computer', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Fellow',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=100)),
+                ('last_name', models.CharField(max_length=100)),
+                ('payment_start_date', models.DateField(null=True)),
+                ('computer', models.ForeignKey(to='computer.Computer')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
         migrations.CreateModel(
             name='StaffUser',
             fields=[
