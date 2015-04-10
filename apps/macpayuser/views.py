06e8e440a87b08e347277370291ede252ec09bfe
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 17:06:39 +0100

    Create Login, Logout, and Dashboard views

--- apps/macpayuser/views.py
+++ apps/macpayuser/views.py
@@ -1,13 +1,58 @@
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
+from django.contrib.auth import login, authenticate, logout
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 
 
-
+# Class Based Home View
 class HomeView(View):
 
     def get(self, request):
-        return render_to_response('index.html')
+        return render_to_response('index.html', locals())
+
+# Class Based Login View
+class LoginView(View):
+
+    def get(self, request):
+        return HttpResponseRedirect(reverse('home'))
+
+    def post(self, request):
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('dashboard'), locals())
+            else:
+                print "nay"
+
+        return HttpResponse('Done')
+
+# Class Based Logout View
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('home'), locals())
+
+
+# Class Based Dashboard View
+class DashboardView(View):
+    def get(self, request):
+        return render_to_response('dashboard.html', locals())
+
+
+
+
+
+
+
+
+
+
+
