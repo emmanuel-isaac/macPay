847da2f2e3b7e9036b30aab752b310c695661385
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 17:07:03 +0100

    Create route for dashboard view

--- macPay/urls.py
+++ macPay/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-from apps.macpayuser.views import HomeView
+from apps.macpayuser.views import HomeView, LoginView, LogoutView, DashboardView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
+    url(r'^dashboard/', DashboardView.as_view(), name='dashboard'),
 )
