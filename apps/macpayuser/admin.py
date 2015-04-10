c5b5c369e26f38f104a39507dc60bf7c04ea8e93
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-10 15:52:36 +0100

    register models in macpayuser app

--- apps/macpayuser/admin.py
+++ apps/macpayuser/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 
-from apps.macpayuser.models import StaffUser
+from apps.macpayuser.models import StaffUser, Fellow
+
 
 # Register your models here.
 admin.site.register(StaffUser)
+admin.site.register(Fellow)
