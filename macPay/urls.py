cc1afaf9034fe3424aa23c250b09741a6d6647ed
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-12 23:10:58 +0100

    include route for payment plan creation

--- macPay/urls.py
+++ macPay/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-from apps.macpayuser.views import HomeView, LoginView, LogoutView, DashboardView
+from apps.macpayuser.views import (
+    HomeView,
+    LoginView,
+    LogoutView,
+    DashboardView,
+)
+from apps.payment.views import CreatePlanView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
@@ -10,4 +16,5 @@ urlpatterns = patterns('',
     url(r'^login/', LoginView.as_view(), name='login'),
     url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^dashboard/', DashboardView.as_view(), name='dashboard'),
+    url(r'^createplan/(?P<pk>\d+)/$', CreatePlanView.as_view(), name='create-plan'),
 )
