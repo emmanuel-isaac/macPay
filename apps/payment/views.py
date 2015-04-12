165061090a0980d0247a6130877a48996976ee06
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-13 00:53:23 +0100

    create Sync Payment View

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+import datetime
 
 
 from apps.macpayuser.models import Fellow
 from apps.payment.forms import PaymentPlanForm
 from apps.computer.models import Computer
+from apps.payment.models import PaymentHistory
 
 
 # Create your views here.
@@ -22,7 +24,6 @@ class CreatePlanView(View):
         return render_to_response('create-plan.html', locals(), context_instance=RequestContext(request))
 
     def post(self, request, pk):
-        print request.POST
         form = PaymentPlanForm(request.POST)
         if form.is_valid():
             form.save()
@@ -32,4 +33,23 @@ class CreatePlanView(View):
             fellow.computer = mac
             fellow.save()
             print request.POST.get('mac', '')
-            return HttpResponse('Done')
+            return HttpResponseRedirect(reverse('create_plan_success'))
+
+def create_plan_success(request):
+    return render_to_response('create-plan-success.html', locals())
+
+class SyncPaymentView(View):
+    def get(self, request):
+        all_fellows = Fellow.objects.all()
+        for fellow in all_fellows:
+            try:
+                last_payment_date = fellow.payment_histories.last().date.strftime('%m %y')
+            except AttributeError, e:
+                last_payment_date = '0 0'
+            current_date = datetime.datetime.now().strftime('%m %y')
+            if last_payment_date != current_date:
+                payment_history = PaymentHistory(fellow=fellow, sum_paid=fellow.monthly_payment, payment_plan=fellow.recent_payment_plan)
+                payment_history.save()
+                print payment_history
+            continue
+        return HttpResponseRedirect(reverse('dashboard'))
