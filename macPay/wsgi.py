113dfacc17bd2a0d7e6123d0c8480e5f5c15ecf7
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-16 01:16:05 +0100

    edit wsgi script to get DJANGO_SETTINGS_MODULE setting

--- macPay/wsgi.py
+++ macPay/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "macPay.settings")
+import envvars
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", envvars.get('DJANGO_SETTINGS_MODULE'))
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
