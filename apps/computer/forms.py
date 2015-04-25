b3bfe53d01f850741b92d7baadd60edf83dac761
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-25 14:18:20 +0100

    Create Model Form for Computer

---
+++ apps/computer/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+
+from apps.computer.models import Computer
+
+
+class ComputerCreationForm(ModelForm):
+
+    class Meta:
+        model = Computer
+        fields = ['name', 'model', 'cost']
