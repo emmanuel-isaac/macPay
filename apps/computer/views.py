cf96390687aec7688138c0c6d43cf954c7f0a6f1
Emmanuel Isaac <emmanuel.isaac@andela.co>
2015-04-25 14:16:19 +0100

    Create EditComputerView, CreateComputerView views Create routes for views Edit computer creation template

--- apps/computer/views.py
+++ apps/computer/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import View
+from django.views.generic.list import ListView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+
+from apps.computer.models import Computer
+from apps.computer.forms import ComputerCreationForm
+
+
+
 
 # Create your views here.
+
+class ComputerListView(ListView):
+
+    model = Computer
+
+    def get_context_data(self, **kwargs):
+        context = super(ComputerListView, self).get_context_data(**kwargs)
+        context['request'] = self.request
+        return context
+
+
+
+class CreateComputerView(View):
+    def get(self, request):
+        form = ComputerCreationForm()
+        request.session['action'] = 'create'
+        return render_to_response('computer_creation.html', locals(), context_instance = RequestContext(request))
+
+    def post(self, request):
+        form = ComputerCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('success'))
+
+        return render_to_response('computer_creation.html', locals(), context_instance=RequestContext(request))
+
+
+class EditComputerView(View):
+    def get(self, request, pk):
+        computer = Computer.objects.get(pk=pk)
+        form = ComputerCreationForm()
+        request.session['action'] = 'edit'
+        return render_to_response('computer_creation.html', locals(), context_instance=RequestContext(request))
+
+    def post(self, request, pk):
+        form = ComputerCreationForm(request.POST)
+        if form.is_valid():
+            computer = Computer.objects.get(pk=pk)
+            computer.name = request.POST.get('name')
+            computer.model = request.POST.get('model')
+            computer.cost = request.POST.get('cost')
+            computer.save()
+            return HttpResponseRedirect(reverse('success'))
+
+        else:
+            return render_to_response('computer_creation.html', locals(), context_instance=RequestContext(request))
+
+
--- macPay/urls.py
+++ macPay/urls.py
@@ -10,6 +10,7 @@ from apps.macpayuser.views import (
     DashboardView,
 )
 from apps.payment.views import CreatePlanView, success, SyncPaymentView, ChangePaymentPlanView
+from apps.computer.views import CreateComputerView, ComputerListView, EditComputerView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
@@ -17,8 +18,11 @@ urlpatterns = patterns('',
     url(r'^login/', LoginView.as_view(), name='login'),
     url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^dashboard/', login_required( DashboardView.as_view() ), name='dashboard'),
-    url(r'^createplan/(?P<pk>\d+)/$', login_required( CreatePlanView.as_view() ), name='create_plan'),
+    url(r'^payment/createplan/(?P<pk>\d+)/$', login_required( CreatePlanView.as_view() ), name='create_plan'),
     url(r'^payment/success/$', success, name='success'),
     url(r'^sync-payment/$', login_required( SyncPaymentView.as_view() ), name='sync_payment'),
     url(r'^payment/change-plan/(?P<pk>\d+)/$', login_required( ChangePaymentPlanView.as_view() ), name='change_plan'),
+    url(r'^computer/list/$', login_required( ComputerListView.as_view() ), name='computer_list'),
+    url(r'^computer/create/$', login_required( CreateComputerView.as_view() ), name='computer_create'),
+    url(r'^computer/edit/(?P<pk>\d+)/$', login_required( EditComputerView.as_view() ), name='computer_edit'),
 )
