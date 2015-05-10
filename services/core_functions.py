5ae3ab67d02d953c2cb25fe1690a0ede6f0a9f9d
andela-eisaac <deejecks@live.com>
2015-05-10 22:59:28 +0100

    create core_functions file and create function there

---
+++ services/core_functions.py
@@ -0,0 +1,9 @@
+def create_payment_history(months, fellow):
+    payment_plan = fellow.recent_payment_plan
+    computer_cost = fellow.computer.cost
+    sum_paid = float(computer_cost) / payment_plan.plan_duration
+    for i in xrange(0, months):
+        payment = PaymentHistory(fellow=fellow, sum_paid=sum_paid, payment_plan=payment_plan)
+        payment.save()
+        continue
+    return 'Created'
