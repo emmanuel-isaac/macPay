fec0bf813e9d8212a3082d287785fb9e9aa42be0
andela-eisaac <deejecks@live.com>
2015-05-11 06:24:29 +0100

    [Bug fixes]

--- services/core_functions.py
+++ services/core_functions.py
@@ -1,8 +1,8 @@
-from apps.payment.models import PaymentHistory
 
 
 
 def create_payment_history(months, fellow):
+    from apps.payment.models import PaymentHistory
     payment_plan = fellow.recent_payment_plan
     computer_cost = fellow.computer.cost
     sum_paid = float(computer_cost) / payment_plan.plan_duration
