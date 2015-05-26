a3e817f9feead905aa30079b273dd201b6697242
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-05-26 20:11:25 +0100

    Edit debug settings

--- macPay/settings/prod.py
+++ macPay/settings/prod.py
@@ -2,6 +2,8 @@ from .base import *
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+import envvars
+envvars.load()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -9,7 +11,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+DEBUG = envvars.get('DEBUG')
 
 TEMPLATE_DEBUG = False
 
