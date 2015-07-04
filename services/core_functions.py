a4e7eade660f4ffb345a2d59f7aedcbfc3a27d7f
andela-eisaac <deejecks@live.com>
2015-07-04 07:00:18 +0100

    [enhancement] Core functions: Include new functions

--- services/core_functions.py
+++ services/core_functions.py
@@ -1,5 +1,6 @@
 from apps.payment.models import PaymentHistory
-
+import calendar
+import datetime
 
 
 def create_payment_history(months, fellow):
@@ -11,3 +12,23 @@ def create_payment_history(months, fellow):
         payment.save()
         continue
     return 'Created'
+
+
+def diff_month(d1, d2):
+    return (d1.year - d2.year) * 12 + d1.month - d2.month
+
+
+def add_months(source_date, months):
+    month = source_date.month - 1 + months
+    year = source_date.year + month / 12
+    month = month % 12 + 1
+    day = min(source_date.day, calendar.monthrange(year, month)[1])
+    return datetime.date(year, month, day)
+
+
+def monthdelta(date, delta):
+    m, y = (date.month + delta) % 12, date.year + ((date.month) + delta - 1) // 12
+    if not m: m = 12
+    d = min(date.day, [31,
+                       29 if y % 4 == 0 and not y % 400 == 0 else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m - 1])
+    return date.replace(day=d, month=m, year=y)
