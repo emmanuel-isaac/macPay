d98395bce4cb2b8e9180c91c17a3b03bbbf751f6
andela-eisaac <deejecks@live.com>
2015-07-04 07:08:24 +0100

    [chore] CreatePlanView, SyncPaymentView, ChangePaymentPlanView: Modify and Simplify Views

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -6,11 +6,11 @@ from django.template import RequestContext
 from django.contrib.auth.decorators import login_required, permission_required
 import datetime
 
-
 from apps.macpayuser.models import Fellow
-from apps.payment.forms import PaymentPlanForm
+from apps.payment.forms import PaymentPlanForm, PaymentHistoryForm
 from apps.computer.models import Computer
 from apps.payment.models import PaymentHistory, PaymentPlan
+from apps.macpayuser.templatetags.macpayuser_extras import get_balance, get_months_left_on_plan
 
 from services.core_functions import create_payment_history
 
@@ -19,51 +19,53 @@ from services.core_functions import create_payment_history
 class CreatePlanView(View):
     def get(self, request, pk):
         fellow = Fellow.objects.get(pk=pk)
-        form = PaymentPlanForm()
-        computers = Computer.objects.all()
-
+        form = PaymentHistoryForm()
         return render_to_response('create-plan.html', locals(), context_instance=RequestContext(request))
 
     def post(self, request, pk):
-        form = PaymentPlanForm(request.POST)
+        form = PaymentHistoryForm(request.POST)
         fellow = Fellow.objects.get(pk=pk)
+        print form.is_valid()
         if form.is_valid():
-            form.save()
-            mac_id = request.POST.get('mac', '')
-            if mac_id:
-                mac = Computer.objects.get(pk=mac_id)
-                fellow.computer = mac
-                fellow.save()
-                try:
-                    prior_payment = int(request.POST.get('prior_pays', ''))
-                    if prior_payment:
-                        create_payment_history(prior_payment, fellow)
-                except Exception, e:
-                    pass
-                request.session['status'] = 'create'
-                return HttpResponseRedirect(reverse('success'))
+            form.save(fellow)
+            request.session['status'] = 'create'
+            return HttpResponseRedirect(reverse('success'))
 
         return render_to_response('create-plan.html', locals(), context_instance=RequestContext(request))
+
 
 @login_required
 def success(request):
     return render_to_response('success.html', locals())
 
+
 class SyncPaymentView(View):
     def get(self, request):
-        all_fellows = Fellow.objects.all()
-        for fellow in all_fellows:
-            try:
-                last_payment_date = fellow.payment_histories.last().date.strftime('%m %y')
-            except AttributeError, e:
-                last_payment_date = ''
-            current_date = datetime.datetime.now().strftime('%m %y')
-            if last_payment_date != current_date and fellow.computer and fellow.recent_payment_plan:
-                payment_history = PaymentHistory(fellow=fellow, sum_paid=fellow.monthly_payment, payment_plan=fellow.recent_payment_plan)
-                payment_history.save()
-                print payment_history
-            continue
+        fellows = Fellow.objects.all()
+        fellows_last_payment = map(lambda x: x.paymenthistory_set.last(), fellows)
+        fellows_balances = map(lambda x: get_balance(x), fellows)
+        payments_per_fellow = map(lambda x: x.paymenthistory_set.all().count(), fellows)
+        for i in xrange(0, len(fellows)):
+            last_payment = fellows_last_payment[i]
+            fellow = fellows[i]
+            if fellows_last_payment[i]:
+                # *------For Fellows with New Plans-------* #
+                if payments_per_fellow[i] == 1 and not last_payment.sum_paid:
+                    monthly_due = float(fellows[i].computer.cost) / last_payment.current_payment_plan.plan_duration
+                    last_payment.sum_paid = monthly_due
+                    fellow.payment_start_date = datetime.date.today()
+                    fellow.save()
+                    last_payment.save()
+                # *------For Fellows that have not paid for current month-------* #
+                elif last_payment.date.strftime('%m %y') != datetime.date.today().strftime('%m %y'):
+                    monthly_due = float(fellows_balances[i]) / get_months_left_on_plan(fellow)
+                    new_history = PaymentHistory(fellow=fellow, sum_paid=monthly_due,
+                                                 current_payment_plan=last_payment.current_payment_plan,
+                                                 previous_payment_plan=last_payment.current_payment_plan)
+                    new_history.save()
+
         return HttpResponseRedirect(reverse('dashboard'))
+
 
 class ChangePaymentPlanView(View):
     def get(self, request, pk):
@@ -75,13 +77,11 @@ class ChangePaymentPlanView(View):
     def post(self, request, pk):
         form = PaymentPlanForm(request.POST)
         fellow = Fellow.objects.get(pk=pk)
-        initial_payment_plan_duration = fellow.payment_plans.last().plan_duration
         if form.is_valid():
-            payment_plan_change_val = request.POST.get('plan_duration', '')
-            date_created = request.POST.get('date_created', '')
-            if initial_payment_plan_duration != payment_plan_change_val:
-                form.save()
-                request.session['status'] = 'change'
-                return HttpResponseRedirect(reverse('success'), locals())
-            else:
-                return HttpResponse('Not Done')
+            data = form.data
+            last_payment = fellow.paymenthistory_set.last()
+            last_payment.current_payment_plan = PaymentPlan.objects.get(plan_duration=data['plan_duration'])
+            last_payment.save()
+            return HttpResponseRedirect(reverse('success'), locals())
+        else:
+            return render_to_response('change-payment-plan.html', locals(), context_instance=RequestContext(request))
