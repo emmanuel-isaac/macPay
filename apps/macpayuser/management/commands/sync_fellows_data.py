434456d0bc76229f51a6db2b938cdebeeea2c44a
andela-eisaac <deejecks@live.com>
2015-08-09 12:26:54 +0100

    [chore] Reduce load time by reducing calculations at model level More efficient algorithms for getting fellows' payment information

--- apps/macpayuser/management/commands/sync_fellows_data.py
+++ apps/macpayuser/management/commands/sync_fellows_data.py
@@ -1,13 +1,8 @@
 from django.core.management.base import BaseCommand
-import webbrowser
-
-
 
 from services.skilltree import *
 
 
 class Command(BaseCommand):
-
-	def handle(self, *args, **options):
-		sync_new_fellows()
-		
+    def handle(self, *args, **options):
+        sync_new_fellows()
--- services/sync_fellows_data.sh
+++ services/sync_fellows_data.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 source ~/.config/zsh/profile
 workon macPay
 python manage.py sync_fellows_data
